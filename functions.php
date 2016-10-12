0dfabf48ca5d747129d102e9aa1640b4b5cf2953
Michael Dean <mike-dean1@hotmail.com>
2016-10-12 21:20:43 +0000

    modifying single services page to be like normal pages

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
+//hook into wordpress to add stylesheets from the parent theme
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+//load stylesheets from parent theme
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
